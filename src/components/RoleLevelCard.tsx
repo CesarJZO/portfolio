0b459635801fba98dee15f0ab692ed3f382d03c8
César Zárate <cesarjzo@outlook.com>
2023-12-19 12:20:33 -0600

    Refactor RoleLevelCard component and CSS
    This commit refactors the RoleLevelCard component and its associated CSS file. The changes include moving the progress bar styles to a separate component and updating the alt text for the role icon.

--- src/components/RoleLevelCard.tsx
+++ src/components/RoleLevelCard.tsx
@@ -1,17 +1,31 @@
 import "./RoleLevelCard.css";
 
+import ProgressBar from "./ProgressBar";
+
+/**
+ * RoleLevelCard component
+ *
+ * This component renders a card that displays the level and experience of a role.
+ *
+ * @param {string} props.role - The name of the role.
+ * @param {number} props.level - The level of the role.
+ * @param {number} props.xp - The amount of experience points of the role.
+ * @param {number} props.progress - The percentage of progress to be displayed. This should be a number between 0 and 100.
+ * @param {string} props.iconPath - The path to the icon of the role.
+ *
+ * @returns The RoleLevelCard component.
+ */
 function RoleLevelCard(props: RoleLevelCardProps) {
   return (
     <section id="role-level-card">
-      <img className="role-icon" src={props.iconPath} alt="Level icon" />
+      <img className="role-icon" src={props.iconPath} alt="Role icon" />
+
       <strong className="exp-level">{props.level}</strong>
+
       <h2 className="exp-title">{props.role}</h2>
-      <div className="progress-bar" role="progressbar">
-        <div
-          className="progress-fill"
-          style={{ width: `${props.progress}%` }}
-        />
-      </div>
+
+      <ProgressBar progress={props.progress} />
+
       <strong className="exp-points">{props.xp} XP</strong>
     </section>
   );
