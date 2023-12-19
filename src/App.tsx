4c5a8f3786c841dc8212711cce87ce72a6ef5e83
César Zárate <cesarjzo@outlook.com>
2023-12-19 16:47:01 -0600

    Refactor social links, roles, and links in App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,66 +4,70 @@ import TopBar from "./components/TopBar";
 import BottomBar from "./components/BottomBar";
 import ProfileCard from "./components/ProfileCard";
 
+const socialLinks = [
+  {
+    site: "GitHub",
+    icon: "src/assets/logo-github.svg",
+    link: "https://github.com/CesarJZO",
+  },
+  {
+    site: "LinkedIn",
+    icon: "src/assets/logo-linkedin.svg",
+    link: "https://www.linkedin.com/in/cesarjzo/",
+  },
+  {
+    site: "Twitter",
+    icon: "src/assets/logo-twitter.svg",
+    link: "https://twitter.com/CesarJZO",
+  },
+];
+
+const roles = [
+  {
+    role: "Frontend Developer",
+    level: 3,
+    xp: 100,
+    progress: 50,
+    icon: "src/assets/game-controller-outline.svg",
+  },
+  {
+    role: "Backend Developer",
+    level: 5,
+    xp: 50,
+    progress: 75,
+    icon: "src/assets/hardware-chip-outline.svg",
+  },
+  {
+    role: "Fullstack Developer",
+    level: 2,
+    xp: 50,
+    progress: 25,
+    icon: "src/assets/cloud-outline.svg",
+  },
+];
+
+const links = [
+  { name: "Projects", path: "/projects" },
+  { name: "Blog", path: "/blog" },
+  { name: "Tutorials", path: "/tutorials" },
+  { name: "Courses", path: "/courses" },
+];
+
 function App() {
   return (
     <>
-      <TopBar
-        socialLinks={[
-          {
-            site: "GitHub",
-            icon: "src/assets/logo-github.svg",
-            link: "https://github.com/CesarJZO",
-          },
-          {
-            site: "LinkedIn",
-            icon: "src/assets/logo-linkedin.svg",
-            link: "https://www.linkedin.com/in/cesarjzo/",
-          },
-          {
-            site: "Twitter",
-            icon: "src/assets/logo-twitter.svg",
-            link: "https://twitter.com/CesarJZO",
-          },
-        ]}
-      />
+      <TopBar socialLinks={socialLinks} />
 
       <main>
         <ProfileCard
           name="César Zárate"
           picture="src/assets/pp-no-bg.png"
-          roles={[
-            {
-              role: "Frontend Developer",
-              level: 3,
-              xp: 100,
-              progress: 50,
-              icon: "src/assets/game-controller-outline.svg",
-            },
-            {
-              role: "Backend Developer",
-              level: 5,
-              xp: 50,
-              progress: 75,
-              icon: "src/assets/hardware-chip-outline.svg",
-            },
-            {
-              role: "Fullstack Developer",
-              level: 2,
-              xp: 50,
-              progress: 25,
-              icon: "src/assets/cloud-outline.svg",
-            },
-          ]}
+          roles={roles}
         />
       </main>
 
       <BottomBar
-        links={[
-          { name: "Projects", path: "/projects" },
-          { name: "Blog", path: "/blog" },
-          { name: "Tutorials", path: "/tutorials" },
-          { name: "Courses", path: "/courses" },
-        ]}
+        links={links}
       />
     </>
   );
